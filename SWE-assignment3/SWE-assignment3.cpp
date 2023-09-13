5d6fb8ff27fed035e26146bf95c4a439eca2c4e3
Emma-Reiff <98106194+Emma-Reiff@users.noreply.github.com>
2023-09-13 13:32:55 -0500

    Add a multiplication method

--- SWE-assignment3/SWE-assignment3.cpp
+++ SWE-assignment3/SWE-assignment3.cpp
@@ -18,3 +18,13 @@ int add(int arr[]) {
 
     return sum;
 }
+
+int multiply(int arr[]) {
+    int sum = 0;
+
+    for (int i = 0; i < sizeof(arr); i++) {
+        sum *= arr[i];
+    }
+
+    return sum;
+}
